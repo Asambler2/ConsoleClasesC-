a98035400f76d318c8cfc074d2c34b798cd44cd7
Asambler2 <chema508@hotmail.com>
2024-05-10 10:08:24 +0200

    Añadido coleccion en una clase Wrapper siguiendo el patron decorador y sumar el total del precio en la coleccion

---
+++ ConsoleClasesC#/Garage/GestionColeccion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClasesC_.Garage
+{
+    public class GestionColeccion
+    {
+        private double Total;
+        private List<IAlquilable> Alquileres = new();
+
+        public void Add(IAlquilable elemento)
+        {
+            this.Alquileres.Add(elemento);
+            this.Total += elemento.DameCoste();
+        }
+
+        public double DameTotal()
+        {
+            return Total;
+        }
+    }
+}
